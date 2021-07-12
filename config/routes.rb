9faadbee13bf64c407838561b9cb1b43ed96e73b
“Amu-Y” <hakumai.morimori@gmail.com>
2021-07-12 06:21:46 +0000

    [Add]メニューレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'users/sessions'
       }
-  scope module: :users do
+  scope module: :public do
     root to: "homes#home"
     get 'about' => 'homes#about'
 
     resources :customers,only: [:edit, :update, :show] 
   end
-  #管理者側
+  #管理者側ルーティング
   devise_for :admins, controllers: {
     sessions: 'admins/sessions'
   }
