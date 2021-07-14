ce4d5297797ed71b7554ca0bd1070b58dcda8b3b
“Amu-Y” <hakumai.morimori@gmail.com>
2021-07-14 23:50:56 +0000

    [Add]チャット機能

--- app/controllers/public/messages_controller.rb
+++ app/controllers/public/messages_controller.rb
@@ -2,17 +2,18 @@ class Public::MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
+    @message.user_id = current_user.id
+
     if @message.save
-      redirect_to room_path(message.room.reservation_id)
+      redirect_to room_path(@message.room_id)
     else
-      redirect_to room_path(message.room.reservation_id), alert: 'メッセージを送信できませんでした'
+      redirect_to room_path(@message.room_id), alert: 'メッセージを送信できませんでした'
     end
   end
-  
+
   private
-  
+
     def message_params
-      params.require(:message).permit(:content, :room_id, :user_id)
+      params.require(:message).permit(:body, :room_id)
     end
-  
 end
--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -2,8 +2,8 @@ class Public::RoomsController < ApplicationController
 
   def create
     if user_signed_in?
-    @room = Room.new(room_params)
-    @room.save
+      @room = Room.new(room_params)
+      @room.save
     else
 
     end
@@ -11,22 +11,15 @@ class Public::RoomsController < ApplicationController
   end
 
   def show
-    @room = Room.find(params[:id]) 
-    @message = Message.new 
-    @messages = @room.messages 
-    
-    if Entry.where(user_id: current_user.id,room_id: @room.id).present?
-      @messages = @room.messages
-      @message = Message.new
-      @entries = @room.entries
-    else
-      redirect_back(fallback_location: root_path)
-    end
+    @room = Room.find(params[:id])
+    @message = Message.new
+    @messages = @room.messages
   end
-  private
   
+  private
+
     def room_params
       params.require(:room).permit(:name)
     end
-  
+
 end
