ab208522278987aeb65374e5bd466b7e87385c9f
“Amu-Y” <hakumai.morimori@gmail.com>
2021-07-26 04:31:10 +0000

    [Update]レイアウト

--- app/controllers/public/messages_controller.rb
+++ app/controllers/public/messages_controller.rb
@@ -19,6 +19,6 @@ class Public::MessagesController < ApplicationController
   private
 
     def message_params
-      params.require(:message).permit(:body, :room_id, :image)
+      params.require(:message).permit(:body, :room_id, :user_id, :image)
     end
 end
